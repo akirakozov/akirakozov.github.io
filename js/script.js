e85e74008d7fe0fae12fdaf1cb885352980bf675
Alexander Kirakozov <akirakozov@yandex-team.ru>
2014-08-19 00:46:16 +0400

    fix scrolling position

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     $(".menu-item").click(function(event) {     
         event.preventDefault();
-        $('html,body').animate({scrollTop:$(this.hash).offset().top-50}, 500);
+        $('html,body').animate({scrollTop:$(this.hash).offset().top-70}, 500);
     }); 
 }
